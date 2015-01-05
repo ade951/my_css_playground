5fa8bce31f55177a631c9b41d743743fe016a140
Cam@mac <camwide88@gmail.com>
2015-01-05 08:56:38 +0800

    add a data config file

---
+++ data/index.js
@@ -0,0 +1,10 @@
+var articles = [
+	{
+		link: 'https://ade951.github.io/my_css_playground/object-fit/', 
+		title: '控制图像纵横比'
+	},
+	{
+		link: 'https://ade951.github.io/my_css_playground/gradient/', 
+		title: '让你的背景亮起来'
+	}
+];
---
+++ javascripts/index.js
@@ -0,0 +1,9 @@
+// include jQuery and ../data/index.js first
+
+$(function(){
+	var content_ul = $('#content_ul');
+	content_ul.html(''); //remove the initial data
+	for(var k in articles){
+		content_ul.append('<li><a href="'+articles[k]['link']+'">'+articles[k]['title']+'</a></li>');
+	}
+});
