aa4b1f4eefe4c3611467e3418ab3662ec7299599
Cam@mac <camwide88@gmail.com>
2015-01-07 01:18:43 +0800

    CSS三动画：选项卡效果

--- data/index.js
+++ data/index.js
@@ -6,5 +6,9 @@ var articles = [
 	{
 		link: 'https://ade951.github.io/my_css_playground/gradient/', 
 		title: '让你的背景亮起来'
+	},
+	{
+		link: 'https://ade951.github.io/my_css_playground/transition/', 
+		title: 'transition动画：选项卡效果'
 	}
 ];
