9bbb92b93f8faab4092357c674300e9b5ade12f4
Cam@mac <camwide88@gmail.com>
2015-04-19 12:25:41 +0800

    1.在新页面打开示例； 2.更改示例的排序。

--- data/index.js
+++ data/index.js
@@ -1,26 +1,26 @@
 var articles = [
 	{
+		link: 'https://ade951.github.io/my_css_playground/tut_images/',
+		title: 'CSS3音乐播放器（可点击）'
+	},
+	{
+		link: 'https://ade951.github.io/my_css_playground/animation/',
+		title: 'animation动画（物体移动效果）'
+	},
+	{
+		link: 'https://ade951.github.io/my_css_playground/animation/flash.html',
+		title: 'animation动画（文字闪烁效果）'
+	},
+	{
+		link: 'https://ade951.github.io/my_css_playground/transition/',
+		title: 'transition动画（选项卡效果）'
+	},
+	{
 		link: 'https://ade951.github.io/my_css_playground/object-fit/', 
 		title: '控制图像纵横比'
 	},
 	{
 		link: 'https://ade951.github.io/my_css_playground/gradient/', 
 		title: '让你的背景亮起来'
-	},
-	{
-		link: 'https://ade951.github.io/my_css_playground/transition/', 
-		title: 'transition动画：选项卡效果'
-	},
-	{
-		link: 'https://ade951.github.io/my_css_playground/animation/', 
-		title: 'animation动画（物体移动效果）'
-	},
-	{
-		link: 'https://ade951.github.io/my_css_playground/animation/flash.html', 
-		title: 'animation动画（文字闪烁效果）'
-	},
-	{
-		link: 'https://ade951.github.io/my_css_playground/tut_images/',
-		title: 'CSS3音乐播放器（可点击）'
 	}
 ];
--- javascripts/index.js
+++ javascripts/index.js
@@ -4,6 +4,6 @@ $(function(){
 	var content_ul = $('#content_ul');
 	content_ul.html(''); //remove the initial data
 	for(var k in articles){
-		content_ul.append('<li><a href="'+articles[k]['link']+'">'+articles[k]['title']+'</a></li>');
+		content_ul.append('<li><a href="'+articles[k]['link']+'" target="_blank">'+articles[k]['title']+'</a></li>');
 	}
 });
